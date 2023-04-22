66f83ccf115f4e86e76b21f8b79ab07f196c0bcc
Jagjit Singh <jagik22@gmail.com>
2023-04-22 12:55:20 -0700

    navigation links

--- frontend/my-app/src/components/MainNavigation.js
+++ frontend/my-app/src/components/MainNavigation.js
@@ -1,3 +1,47 @@
-const MainNavigation = () => {};
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <nav className={classes.nav}>
+      <ul className={classes.list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/bootcamps"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Bootcamps
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default MainNavigation;
