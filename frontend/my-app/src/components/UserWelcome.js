c31822930f144a23cdd52a8d3cd5a652bb7e2638
Jagjit Singh <jagik22@gmail.com>
2023-05-30 18:26:12 -0700

    small changes

--- frontend/my-app/src/components/UserWelcome.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../store/auth-context";
-
-const UserWelcome = () => {
-  const authContext = useContext(AuthContext);
-  return <h1>Welcome, {authContext.firstName}!</h1>;
-};
-
-export default UserWelcome;
